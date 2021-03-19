7e052c43c80ea83e2217bf62e192b4256dbad04b
Ahmet Can Duyar <Acan.d@outlook.com>
2021-03-19 19:49:07 +0300

    .

--- CacheCallbackAndDepencyTest/Controllers/HomeController.cs
+++ CacheCallbackAndDepencyTest/Controllers/HomeController.cs
@@ -1,33 +1,21 @@
-﻿using System;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Web.Mvc;
 
 namespace CacheCallbackAndDepencyTest.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpPost]
-        public JsonResult Control()
-        {
-            return Json(MC._1second.Updatable(
-                "name",
-                DateTime.Now.AddSeconds(5),
-                false,
-                () => $"{DateTime.Now:HH:mm:ss}"
-            ));
-        }
-        public ActionResult Index() => View();
-        public ActionResult Action() => View();
-        public ActionResult FrameMain() => View();
+        public ActionResult TestPage() => View();
+        protected new ViewResult View([CallerMemberName] string name = null) => View(name, "~/Views/Layout.cshtml");
+        #region FRAMES
+        [OutputCache(Duration = 600, VaryByParam = "*")]
         public ActionResult Frame1() => View();
+        [OutputCache(Duration = 1800, VaryByParam = "*")]
         public ActionResult Frame2() => View();
+        [OutputCache(Duration = 3000, VaryByParam = "*")]
         public ActionResult Frame3() => View();
+        [OutputCache(Duration = 9000, VaryByParam = "*")]
         public ActionResult Frame4() => View();
-        protected new ViewResult View(bool usLayout = false, [CallerMemberName] string name = null)
-        {
-            var vr = View(name);
-            vr.MasterName = "~/Views/Layout.cshtml";
-            return vr;
-        }
+        #endregion
     }
 }
